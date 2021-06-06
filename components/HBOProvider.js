459a8543874da3e1d0f3a092c6c18b0abbca3d96
moses555 <moses555@gmail.com>
2021-06-06 08:08:34 -0700

    first commit

---
+++ components/HBOProvider.js
@@ -0,0 +1,39 @@
+import React, { useContext, useState} from 'react';
+
+export const StateContext = React.createContext();
+
+export function useStateContext(){
+  return useContext(StateContext)
+}
+
+export function HBOProvider({children}){
+  const [user, setUser] = useState('')
+  const defaultUserImg = 'https://uifaces.co/our-content/donated/vIqzOHXj.jpg'
+  const createUserAction = (e) => {
+    setUser(e.target.value)
+    console.log(2+2)
+  }
+  const [sideNavOpen, setSideNavOpenAction] = useState(false)
+  const [accountModalOpen, setAccountModalOpenAction] = useState(false)
+  const [searchOpen, setSearchOpenAction] = useState(false)
+
+  const thumbTypes = ['large-v', 'small-v', 'large-h', 'small-h']
+
+  return(
+    <StateContext.Provider
+    value={{
+      user,
+      createUserAction,
+      defaultUserImg,
+      sideNavOpen,
+      setSideNavOpenAction,
+      accountModalOpen,
+      setAccountModalOpenAction,
+      searchOpen, 
+      setSearchOpenAction,
+      thumbTypes
+    }}>
+      {children}
+    </StateContext.Provider>
+  )
+}
---
+++ components/UI/SideNav/SideNav.js
@@ -0,0 +1,42 @@
+import {useStateContext} from '../../HBOProvider'
+import Link from 'next/link';
+
+const SideNav = (props) => {
+	const globalState = useStateContext();
+	return (
+		<div className={`side-nav ${globalState.sideNavOpen ? 'side-nav--active': ''}`}>
+			<div className="side-nav__close-btn" onClick={() => globalState.setSideNavOpenAction(false)}>
+				<i className="fas fa-times" />
+			</div>
+			<ul className="side-nav__main">
+				<li onClick={() => globalState.setSideNavOpenAction(false)}>
+					<Link href="/">
+						<a>
+							Home
+						</a>
+					</Link>
+				</li>
+				<li onClick={() => globalState.setSideNavOpenAction(false)}>
+					<Link href="/movie">
+						<a>
+							Movies
+						</a>
+					</Link>
+				</li>
+				<li onClick={() => globalState.setSideNavOpenAction(false)}>
+					<Link href="/tv">
+						<a>
+							Series
+						</a>
+					</Link>
+				</li>
+				
+				
+				
+			</ul>
+			
+		</div>
+	);
+};
+
+export default SideNav;
